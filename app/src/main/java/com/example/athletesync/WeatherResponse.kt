2842d9997df295e358c29409ef49322b247b7219
Liyakhanya Kitsili <ST10279731@vcconnect.edu.za>
2025-11-03 19:48:10 +0200

    Weather widget in dashboard

--- app/src/main/java/com/example/athletesync/WeatherResponse.kt
+++ app/src/main/java/com/example/athletesync/WeatherResponse.kt
@@ -1,4 +1,38 @@
 package com.example.athletesync
 
-class WeatherResponse {
-}
+
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherResponse(
+    @SerializedName("main") val main: Main,
+    @SerializedName("weather") val weather: List<Weather>,
+    @SerializedName("name") val cityName: String,
+    @SerializedName("wind") val wind: Wind
+)
+
+data class Main(
+    @SerializedName("temp") val temperature: Double,
+    @SerializedName("feels_like") val feelsLike: Double,
+    @SerializedName("humidity") val humidity: Int,
+    @SerializedName("pressure") val pressure: Int
+)
+
+data class Weather(
+    @SerializedName("main") val main: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("icon") val icon: String
+)
+
+data class Wind(
+    @SerializedName("speed") val speed: Double
+)
+data class WeatherData(
+    val temperature: Int,
+    val condition: String,
+    val description: String,
+    val city: String,
+    val humidity: Int,
+    val windSpeed: Int,
+    val iconCode: String
+)

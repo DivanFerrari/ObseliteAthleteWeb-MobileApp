26175105d5df445a38db0acf80c897d5dcfa3be8
Liyakhanya Kitsili <ST10279731@vcconnect.edu.za>
2025-11-03 20:19:23 +0200

    BMI calculator , fixed error

--- app/src/main/java/com/example/athletesync/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/athletesync/BmiCalculatorActivity.kt
@@ -1,9 +1,11 @@
 package com.example.athletesync
 
+
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.athletesync.databinding.ActivityBmiCalculatorBinding
+
 import kotlin.math.pow
 
 class BmiCalculatorActivity : AppCompatActivity() {

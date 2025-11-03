1a2906a89cc103d5daa44642015e97f4173c2b89
Liyakhanya Kitsili <ST10279731@vcconnect.edu.za>
2025-11-03 17:51:03 +0200

    setup onClickListener for bmi calculator

--- app/src/main/java/com/example/athletesync/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/athletesync/BmiCalculatorActivity.kt
@@ -8,14 +8,28 @@ import kotlin.math.pow
 
 
 class BmiCalculatorActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBmiCalculatorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_bmi_calculator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityBmiCalculatorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.calculateButton.setOnClickListener {
+            calculateBMI()
+        }
+
+        binding.resetButton.setOnClickListener {
+            resetCalculator()
+        }
+
+        // Add back button click listener
+        binding.backButton.setOnClickListener {
+            onBackPressed()
         }
     }
-}

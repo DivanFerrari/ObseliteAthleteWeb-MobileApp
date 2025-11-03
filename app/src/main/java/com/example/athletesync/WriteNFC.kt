747a90b563175064a9eff89fad363ee593580d6a
Liyakhanya Kitsili <ST10279731@vcconnect.edu.za>
2025-11-03 18:31:59 +0200

    Incident reporting

--- app/src/main/java/com/example/athletesync/WriteNFC.kt
+++ app/src/main/java/com/example/athletesync/WriteNFC.kt
@@ -1,10 +1,20 @@
 package com.example.athletesync
 
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.Ndef
+import android.nfc.tech.NdefFormatable
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.athletesync.databinding.ActivityWriteNfcBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WriteNFC : AppCompatActivity() {
 

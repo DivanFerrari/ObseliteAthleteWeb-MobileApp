e984886d9bad12b61a3567678e2d779ebf9fcf98
Liyakhanya Kitsili <ST10279731@vcconnect.edu.za>
2025-11-03 19:34:13 +0200

    Read NFC functionality complete

--- app/src/main/java/com/example/athletesync/ReadNFC.kt
+++ app/src/main/java/com/example/athletesync/ReadNFC.kt
@@ -11,7 +11,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.example.wilprototype.databinding.ActivityReadNfcBinding
+import com.example.athletesync.databinding.ActivityReadNfcBinding
+
 
 class ReadNFC : AppCompatActivity() {
 
